9b42f58d58e7ab97d407a7b951538a29166eb4f2
Danniel-Ang <danniel@ui.ac.id>
2025-04-11 19:55:33 +0700

    Red: Making test for user request creation

---
+++ src/test/java/id/ac/ui/cs/advprog/everest/requestServiceAcceptance/model/IncomingRequestTest.java
@@ -0,0 +1,15 @@
+package id.ac.ui.cs.advprog.everest.requestServiceAcceptance.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserRequestTest {
+    @Test
+    void testUserRequestCreation() {
+        UserRequest request = new UserRequest(100L, "Fix washing machine");
+
+        // Assert
+        assertEquals(100L, request.getId());
+        assertEquals("Fix washing machine", request.getUserDescription());
+    }
+}
