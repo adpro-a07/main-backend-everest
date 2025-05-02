81f064536b6d12460aebfe941c0620b1d711182a
Danniel-Ang <danniel@ui.ac.id>
2025-05-02 20:36:27 +0700

    Green: Implement Service for UserRequestService

--- src/main/java/id/ac/ui/cs/advprog/everest/requestServiceAcceptance/service/UserRequestService.java
+++ src/main/java/id/ac/ui/cs/advprog/everest/requestServiceAcceptance/service/UserRequestService.java
@@ -1,4 +1,8 @@
 package id.ac.ui.cs.advprog.everest.requestServiceAcceptance.service;
 
+import id.ac.ui.cs.advprog.everest.requestServiceAcceptance.models.UserRequest;
+
+import java.util.List;
+
 public interface UserRequestService {
 }
