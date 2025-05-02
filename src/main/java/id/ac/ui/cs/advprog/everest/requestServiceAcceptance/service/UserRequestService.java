9986cdc0f4739039136bfcf17a46b2057b7ce9b8
Danniel-Ang <danniel@ui.ac.id>
2025-05-02 20:39:04 +0700

    Green: Implement Interface Service for UserRequestService

--- src/main/java/id/ac/ui/cs/advprog/everest/requestServiceAcceptance/service/UserRequestService.java
+++ src/main/java/id/ac/ui/cs/advprog/everest/requestServiceAcceptance/service/UserRequestService.java
@@ -3,6 +3,18 @@ package id.ac.ui.cs.advprog.everest.requestServiceAcceptance.service;
 import id.ac.ui.cs.advprog.everest.requestServiceAcceptance.models.UserRequest;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRequestService {
+    UserRequest createRequest(String description);
+
+    UserRequest createRequest(UserRequest userRequest);
+
+    Optional<UserRequest> getRequestById(Long id);
+
+    List<UserRequest> getAllRequests();
+
+    boolean deleteRequestById(Long id);
+
+    UserRequest createWithId(UserRequest request);
 }
