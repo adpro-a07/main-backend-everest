86c1f1017e1ac3141198fe15ab93eea7bfeefd30
khansakhai <kak.khansa@gmail.com>
2025-05-02 19:43:26 +0700

    feat: [REFACTOR] Add UpdateRatingRequest for DTO

---
+++ src/main/java/id/ac/ui/cs/advprog/everest/dto/UpdateRatingRequest.java
@@ -0,0 +1,11 @@
+package id.ac.ui.cs.advprog.everest.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateRatingRequest {
+    private String comment;
+    private int rating;
+}
