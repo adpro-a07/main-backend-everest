6a66fad5283913401efcdf6c7082b98408b69816
ryuun1corn <yuda.arif.prasojo@gmail.com>
2025-05-02 15:46:40 +0700

    feat(grpc): implement grpc client to communicate with kilimanjaro.

--- src/main/java/id/ac/ui/cs/advprog/everest/EverestApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/everest/EverestApplication.java
@@ -1,8 +1,10 @@
 package id.ac.ui.cs.advprog.everest;
 
+import id.ac.ui.cs.advprog.everest.service.TestService;
 import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class EverestApplication {
@@ -13,7 +15,9 @@ public class EverestApplication {
                 System.setProperty(entry.getKey(), entry.getValue())
         );
 
-        SpringApplication.run(EverestApplication.class, args);
+        ApplicationContext context = SpringApplication.run(EverestApplication.class, args);
+        TestService service = context.getBean(TestService.class);
+        service.getHello();
     }
 
 }
