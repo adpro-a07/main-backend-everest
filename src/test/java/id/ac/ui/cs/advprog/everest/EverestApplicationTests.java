5ba84b605119092889a01b24cbeabb86e272f804
Faridhsbi <mhmmd.faridhsbi@gmail.com>
2025-05-02 17:27:19 +0700

    test: [RED] Add tests for Coupon Repository

--- src/test/java/id/ac/ui/cs/advprog/everest/EverestApplicationTests.java
+++ src/test/java/id/ac/ui/cs/advprog/everest/EverestApplicationTests.java
@@ -2,6 +2,9 @@ package id.ac.ui.cs.advprog.everest;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootTest
 class EverestApplicationTests {
@@ -10,4 +13,12 @@ class EverestApplicationTests {
     void contextLoads() {
     }
 
+    @RestController
+    @RequestMapping("/")
+    public static class TestController {
+        @GetMapping("/health")
+        public String getHealth() {
+            return "Health is ok!";
+        }
+    }
 }
