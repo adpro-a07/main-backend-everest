76caeb11f39602ce4e26c09b1e93e0c2e83b5807
ryuun1corn <yuda.arif.prasojo@gmail.com>
2025-05-06 17:54:32 +0700

    feat[GREEN]: implement AuthServiceGrpcClient with GrpcConfig class.

---
+++ src/main/java/id/ac/ui/cs/advprog/everest/config/GrpcConfig.java
@@ -0,0 +1,50 @@
+package id.ac.ui.cs.advprog.everest.config;
+
+import id.ac.ui.cs.advprog.everest.service.AuthServiceGrpcClient;
+import id.ac.ui.cs.advprog.everest.util.RequestMetadataUtil;
+import id.ac.ui.cs.advprog.kilimanjaro.auth.grpc.AuthServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class GrpcConfig {
+    @Value("${auth.grpc.secure:false}")
+    private boolean useSecure;
+
+    private ManagedChannel managedChannel;
+
+    @Bean
+    public ManagedChannel authServiceChannel(
+            @Value("${auth.grpc.host}") String host,
+            @Value("${auth.grpc.port}") int port
+    ) {
+        this.managedChannel = useSecure
+                ? ManagedChannelBuilder.forAddress(host, port).useTransportSecurity().build()
+                : ManagedChannelBuilder.forAddress(host, port).usePlaintext().build();
+        return this.managedChannel;
+    }
+
+    @Bean
+    public AuthServiceGrpc.AuthServiceBlockingStub authServiceBlockingStub(ManagedChannel channel) {
+        return AuthServiceGrpc.newBlockingStub(channel);
+    }
+
+    @Bean
+    public AuthServiceGrpcClient authServiceClient(
+            AuthServiceGrpc.AuthServiceBlockingStub stub,
+            RequestMetadataUtil requestMetadataUtil
+    ) {
+        return new AuthServiceGrpcClient(stub, requestMetadataUtil);
+    }
+
+    @PreDestroy
+    public void shutdownChannel() {
+        if (this.managedChannel != null && !this.managedChannel.isShutdown()) {
+            this.managedChannel.shutdown();
+        }
+    }
+}
