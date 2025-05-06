2b262268cf2f7869f04640d0c49547030bb18e7b
Faridhsbi <mhmmd.faridhsbi@gmail.com>
2025-05-06 21:31:09 +0700

    feat: [REFACTOR] apply report status enums

--- src/main/java/id/ac/ui/cs/advprog/everest/modules/repairorder/model/enums/ReportStatus.java
+++ src/main/java/id/ac/ui/cs/advprog/everest/modules/repairorder/model/enums/ReportStatus.java
@@ -1,4 +1,12 @@
 package id.ac.ui.cs.advprog.everest.model.enums;
 
 public enum ReportStatus {
+    PENDING,
+    VERIFIED,
+    REJECTED,
+    COMPLETED;
+
+    public boolean isEmpty() {
+        return this == REJECTED || this == COMPLETED;
+    }
 }
