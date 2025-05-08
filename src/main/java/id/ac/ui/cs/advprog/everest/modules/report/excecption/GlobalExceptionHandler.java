5695f68e607aab70fca3332344cebcd0b52354f3
Faridhsbi <mhmmd.faridhsbi@gmail.com>
2025-05-08 18:49:25 +0700

    feat[GREEN]: add GlobalExcecptionHandler for Report Test

---
+++ src/main/java/id/ac/ui/cs/advprog/everest/modules/report/excecption/GlobalExceptionHandler.java
@@ -0,0 +1,34 @@
+package id.ac.ui.cs.advprog.everest.modules.report.excecption;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.MediaType;
+import org.springframework.http.ProblemDetail;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String,Object>> handleRSE(ResponseStatusException ex,
+                                                        HttpServletRequest req) {
+        ProblemDetail pd = ex.getBody();
+        String reason = pd.getTitle();
+        int code = pd.getStatus();
+        String detail = ex.getReason();
+
+        Map<String,Object> body = Map.of(
+                "status",  code,
+                "error",   reason,
+                "message", detail,
+                "path",    req.getRequestURI()
+        );
+
+        return ResponseEntity.status(code)
+                .contentType(MediaType.APPLICATION_PROBLEM_JSON)
+                .body(body);
+    }
+}
