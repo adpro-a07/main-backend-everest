e6a4c575bbf21e928ed89c1255a16c03f1c90c30
khansakhai <kak.khansa@gmail.com>
2025-05-08 23:24:39 +0700

    [REFACTOR] replace custom RatingRepositoryImpl with JPA-based repository

--- src/main/java/id/ac/ui/cs/advprog/everest/modules/rating/repository/RatingRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/everest/modules/rating/repository/RatingRepository.java
@@ -1,14 +1,14 @@
 package id.ac.ui.cs.advprog.everest.modules.rating.repository;
 
 import id.ac.ui.cs.advprog.everest.modules.rating.model.Rating;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.UUID;
 
-public interface RatingRepository {
-    Rating save(Rating rating);
-    Optional<Rating> findById(UUID id);
-    List<Rating> findAll();
-    void deleteById(UUID id);
+@Repository
+public interface RatingRepository extends JpaRepository<Rating, UUID> {
+    List<Rating> findAllByTechnicianId(String technicianId);
+    List<Rating> findAllByUserId(String userId);
 }
