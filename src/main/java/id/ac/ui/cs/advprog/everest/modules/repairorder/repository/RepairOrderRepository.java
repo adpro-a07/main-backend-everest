97b51788bb1a863420523cacc64a468b005bffc1
ryuun1corn <yuda.arif.prasojo@gmail.com>
2025-05-08 22:27:25 +0700

    refactor: remove unused repair order repository method for now.

--- src/main/java/id/ac/ui/cs/advprog/everest/modules/repairorder/repository/RepairOrderRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/everest/modules/repairorder/repository/RepairOrderRepository.java
@@ -5,13 +5,12 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 public interface RepairOrderRepository extends JpaRepository<RepairOrder, Long> {
     Optional<RepairOrder> findById(@NotBlank @Size(max=100) UUID id);
 
-    Optional<RepairOrder> findByCustomerId(@NotBlank @Size(max=100) UUID customerId);
-
-    Optional<RepairOrder> findByTechnicianId(@NotBlank @Size(max=100) UUID technicianId);
+    List<RepairOrder> findByCustomerId(@NotBlank @Size(max=100) UUID customerId);
 }
