804122cef452250abfda9a7b3bf554abf7ad832c
ryuun1corn <yuda.arif.prasojo@gmail.com>
2025-05-08 11:03:23 +0700

    feat: add custom exception classes.

---
+++ src/main/java/id/ac/ui/cs/advprog/everest/common/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.advprog.everest.common.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/id/ac/ui/cs/advprog/everest/common/exception/UnauthorizedAccessException.java
@@ -0,0 +1,7 @@
+package id.ac.ui.cs.advprog.everest.common.exception;
+
+public class UnauthorizedAccessException extends RuntimeException {
+    public UnauthorizedAccessException(String message) {
+        super(message);
+    }
+}
