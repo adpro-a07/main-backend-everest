19d6bd64ab1dd4e12d8e09dcfd44c9cdd72203b5
ryuun1corn <yuda.arif.prasojo@gmail.com>
2025-05-08 11:16:26 +0700

    feat: define RepairOrderRepository.

---
+++ src/main/java/id/ac/ui/cs/advprog/everest/modules/repairorder/repository/RepairOrderRepository.java
@@ -0,0 +1,17 @@
+package id.ac.ui.cs.advprog.everest.modules.repairorder.repository;
+
+import id.ac.ui.cs.advprog.everest.modules.repairorder.model.RepairOrder;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface RepairOrderRepository extends JpaRepository<RepairOrder, Long> {
+    Optional<RepairOrder> findById(@NotBlank @Size(max=100) UUID id);
+
+    Optional<RepairOrder> findByCustomerId(@NotBlank @Size(max=100) UUID customerId);
+
+    Optional<RepairOrder> findByTechnicianId(@NotBlank @Size(max=100) UUID technicianId);
+}
