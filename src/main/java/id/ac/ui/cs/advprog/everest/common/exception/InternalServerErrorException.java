208331996add8612eef9b134cd51e2dab20ecf42
ryuun1corn <yuda.arif.prasojo@gmail.com>
2025-05-08 11:15:37 +0700

    refactor: remove InternalServerErrorException.

--- src/main/java/id/ac/ui/cs/advprog/everest/common/exception/InternalServerErrorException.java
+++
@@ -1,7 +0,0 @@
-package id.ac.ui.cs.advprog.everest.common.exception;
-
-public class InternalServerErrorException extends RuntimeException {
-    public InternalServerErrorException(String message) {
-        super(message);
-    }
-}
