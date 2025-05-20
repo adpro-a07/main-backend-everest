017961000a79c47efe226e8e949df74920ddb094
Danniel-Ang <danniel@ui.ac.id>
2025-05-21 01:53:05 +0700

    [RED] Recreating Report Services for submit draft tech Report

--- src/main/java/id/ac/ui/cs/advprog/everest/modules/technicianReport/service/TechnicianReportServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/everest/modules/technicianReport/service/TechnicianReportServiceImpl.java
@@ -187,6 +187,8 @@ public class TechnicianReportServiceImpl implements TechnicianReportService {
         }
     }
 
+
+
 //    @Override
 //    public GenericResponse<List<TechnicianReportDraftResponse>> getTechnicianReportByStatusForTechnician(String status, AuthenticatedUser technician) {
 //        if (technician == null) return new GenericResponse<>(false, "Technician cannot be null", null);
@@ -202,39 +204,6 @@ public class TechnicianReportServiceImpl implements TechnicianReportService {
 //        }
 //    }
 
-//
-//    @Override
-//    public GenericResponse<TechnicianReportDraftResponse> deleteTechnicianReportDraft(
-//            String technicianReportDraftId,
-//            AuthenticatedUser technician) {
-//
-//        if (technicianReportDraftId == null || technician == null) {
-//            return new GenericResponse<>(false, "Report data or technician cannot be null", null);
-//        }
-//
-//        try {
-//            TechnicianReport technicianReport = technicianReportRepository.findByReportId(UUID.fromString(technicianReportDraftId))
-//                    .orElseThrow(() -> new InvalidTechnicianReportStateException("Technician report not found"));
-//
-//            if (!technicianReport.getTechnicianId().equals(technician.id())) {
-//                throw new InvalidTechnicianReportStateException("You are not authorized to delete this report");
-//            }
-//
-//            if (!"DRAFT".equals(technicianReport.getStatus())) {
-//                throw new InvalidTechnicianReportStateException("Only report drafts can be deleted");
-//            }
-//
-//            TechnicianReportDraftResponse response = buildTechnicianReportDraftResponse(technicianReport);
-//
-//            technicianReportRepository.delete(technicianReport);
-//
-//            return new GenericResponse<>(true, "Technician report draft deleted successfully", response);
-//        } catch (IllegalArgumentException | DataAccessException | InvalidTechnicianReportStateException |
-//                 IllegalStateTransitionException ex) {
-//            return new GenericResponse<>(false, ex.getMessage(), null);
-//        }
-//    }
-//
 //    @Override
 //    public GenericResponse<TechnicianReportDraftResponse> startWork(
 //            String technicianReportDraftId,
