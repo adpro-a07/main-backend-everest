4bb4dcdc9e3b56f46b4bc34f0fd83d5d84cf9691
Danniel-Ang <danniel@ui.ac.id>
2025-05-23 03:13:46 +0700

    [REFACTOR] Implementign a little clean code

--- src/main/java/id/ac/ui/cs/advprog/everest/modules/technicianReport/dto/CreateTechnicianReportDraftRequest.java
+++ src/main/java/id/ac/ui/cs/advprog/everest/modules/technicianReport/dto/CreateTechnicianReportDraftRequest.java
@@ -1,5 +1,7 @@
 package id.ac.ui.cs.advprog.everest.modules.technicianReport.dto;
 
+import id.ac.ui.cs.advprog.everest.modules.technicianReport.exception.InvalidDataTechnicianReport;
+
 import lombok.Getter;
 import lombok.Setter;
 import lombok.Builder;
@@ -16,21 +18,22 @@ public class CreateTechnicianReportDraftRequest {
     private BigDecimal estimatedCost;
     private Long estimatedTimeSeconds;
 
+
     public void validate() {
         if (repairOrderId == null || repairOrderId.isBlank()) {
-            throw new IllegalArgumentException("repairOrderId cannot be null or blank");
+            throw new InvalidDataTechnicianReport("repairOrderId cannot be null or blank");
         }
         if (diagnosis == null || diagnosis.isBlank()) {
-            throw new IllegalArgumentException("diagnosis cannot be null or blank");
+            throw new InvalidDataTechnicianReport("diagnosis cannot be null or blank");
         }
         if (actionPlan == null || actionPlan.isBlank()) {
-            throw new IllegalArgumentException("actionPlan cannot be null or blank");
+            throw new InvalidDataTechnicianReport("actionPlan cannot be null or blank");
         }
         if (estimatedCost == null || estimatedCost.compareTo(BigDecimal.ZERO) < 0) {
-            throw new IllegalArgumentException("estimatedCost cannot be null or negative");
+            throw new InvalidDataTechnicianReport("estimatedCost cannot be null or negative");
         }
         if (estimatedTimeSeconds == null || estimatedTimeSeconds <= 0) {
-            throw new IllegalArgumentException("estimatedTimeSeconds must be positive");
+            throw new InvalidDataTechnicianReport("estimatedTimeSeconds must be positive");
         }
     }
 }
