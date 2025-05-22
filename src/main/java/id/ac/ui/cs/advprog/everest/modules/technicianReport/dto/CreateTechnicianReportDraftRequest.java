5162d2b718cd014619f22185b298486b6844b096
Danniel-Ang <danniel@ui.ac.id>
2025-05-23 01:57:25 +0700

    [REFACTOR] Updating DTO: technician Report

--- src/main/java/id/ac/ui/cs/advprog/everest/modules/technicianReport/dto/CreateTechnicianReportDraftRequest.java
+++ src/main/java/id/ac/ui/cs/advprog/everest/modules/technicianReport/dto/CreateTechnicianReportDraftRequest.java
@@ -2,13 +2,15 @@ package id.ac.ui.cs.advprog.everest.modules.technicianReport.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.Builder;
 
 import java.math.BigDecimal;
 
+@Builder
 @Getter
 @Setter
-public class CreateTechnicianReportDraft {
-    private String userRequestId;
+public class CreateTechnicianReportDraftRequest {
+    private String repairOrderId;
     private String diagnosis;
     private String actionPlan;
     private BigDecimal estimatedCost;
