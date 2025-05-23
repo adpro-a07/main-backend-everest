f7b314366789fd431084235cb895b8afec72b59b
ryuun1corn <yuda.arif.prasojo@gmail.com>
2025-05-23 11:30:40 +0700

    refactor: remove generic wildcard from controller return values and fix code smells.

--- src/main/java/id/ac/ui/cs/advprog/everest/modules/repairorder/controller/RepairOrderController.java
+++ src/main/java/id/ac/ui/cs/advprog/everest/modules/repairorder/controller/RepairOrderController.java
@@ -25,7 +25,7 @@ public class RepairOrderController {
 
     @PreAuthorize("hasRole('CUSTOMER')")
     @PostMapping("/repair-orders")
-    public ResponseEntity<?> createRepairOrder(
+    public ResponseEntity<GenericResponse<ViewRepairOrderResponse>> createRepairOrder(
             @Valid @RequestBody CreateAndUpdateRepairOrderRequest createAndUpdateRepairOrderRequest,
             @CurrentUser AuthenticatedUser user
     ) {
@@ -36,14 +36,14 @@ public class RepairOrderController {
 
     @PreAuthorize("hasRole('CUSTOMER')")
     @GetMapping("/repair-orders")
-    public ResponseEntity<?> getRepairOrders(@CurrentUser AuthenticatedUser user) {
+    public ResponseEntity<GenericResponse<List<ViewRepairOrderResponse>>> getRepairOrders(@CurrentUser AuthenticatedUser user) {
         GenericResponse<List<ViewRepairOrderResponse>> response = repairOrderService.getRepairOrders(user);
         return ResponseEntity.ok(response);
     }
 
     @PreAuthorize("hasRole('CUSTOMER')")
     @GetMapping("/repair-orders/{repairOrderId}")
-    public ResponseEntity<?> getRepairOrderById(
+    public ResponseEntity<GenericResponse<ViewRepairOrderResponse>> getRepairOrderById(
             @PathVariable String repairOrderId,
             @CurrentUser AuthenticatedUser user
     ) {
@@ -53,7 +53,7 @@ public class RepairOrderController {
 
     @PreAuthorize("hasRole('CUSTOMER')")
     @PutMapping("/repair-orders/{repairOrderId}")
-    public ResponseEntity<?> updateRepairOrder(
+    public ResponseEntity<GenericResponse<ViewRepairOrderResponse>> updateRepairOrder(
             @PathVariable String repairOrderId,
             @Valid @RequestBody CreateAndUpdateRepairOrderRequest createAndUpdateRepairOrderRequest,
             @CurrentUser AuthenticatedUser user
@@ -65,7 +65,7 @@ public class RepairOrderController {
 
     @PreAuthorize("hasRole('CUSTOMER')")
     @DeleteMapping("/repair-orders/{repairOrderId}")
-    public ResponseEntity<?> deleteRepairOrder(
+    public ResponseEntity<GenericResponse<Void>> deleteRepairOrder(
             @PathVariable String repairOrderId,
             @CurrentUser AuthenticatedUser user
     ) {
